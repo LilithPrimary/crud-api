53df2b2aa78820700d33090cae8bffecca667c7c
Denna <lilithprimary@gmail.com>
2023-01-15 13:33:48 +0100

    fix: body validator

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,89 @@
-import { createServer } from 'http';
+import { createServer, IncomingMessage, ServerResponse } from 'http';
+
 import { PORT } from '../constants/PORT';
 import { Color } from '../types/Color';
 import { colorizeText } from '../utils/colorizeText';
-import { serverHandler } from './serverHandler';
+import { ErrorMsg } from '../types/ErrorMsg';
+import { Response } from '../types/IResponse';
+import { IUser } from '../types/IUser';
+import { Methods } from '../types/Methods';
+import { StatusCode } from '../types/StatusCode';
+import { errorHandler } from '../utils/errorHandler';
+import { getRequestBody } from '../utils/getRequestBody';
+import { deleteHandler } from '../utils/methodsHandler/deleteHandler';
+import { getHandler } from '../utils/methodsHandler/getHandler';
+import { postHandler } from '../utils/methodsHandler/postHandler';
+import { putHandler } from '../utils/methodsHandler/putHandler';
+import { URLValidator } from '../utils/URLValidator';
 
 export const app = () => {
-  const server = createServer(serverHandler);
+  let users: IUser[] = [];
+
+  const server = createServer(async (req: IncomingMessage, res: ServerResponse) => {
+    let response: Response = null;
+    let code: number;
+    const { url } = req;
+    const method = <Methods>req.method;
+    res.setHeader('Content-Type', 'application/json');
+
+    try {
+      URLValidator(url, method);
+
+      switch (method) {
+        case Methods.GET:
+          console.log(colorizeText(Color.lightblue, `[${Methods.GET}]`), colorizeText(Color.yellow, `${url}`));
+
+          code = StatusCode.Successful200;
+
+          response = getHandler(url, users);
+          break;
+
+        case Methods.POST: {
+          const body = await getRequestBody(req);
+
+          console.log(colorizeText(Color.lightblue, `[${Methods.POST}]`), colorizeText(Color.yellow, `${url}`), body);
+
+          code = StatusCode.Successful201;
+
+          response = postHandler(users, body);
+          break;
+        }
+
+        case Methods.PUT: {
+          const body = await getRequestBody(req);
+
+          console.log(colorizeText(Color.lightblue, `[${Methods.PUT}]`), colorizeText(Color.yellow, `${url}`), body);
+
+          response = putHandler(url, users, body);
+
+          code = StatusCode.Successful200;
+
+          break;
+        }
+
+        case Methods.DELETE: {
+          console.log(colorizeText(Color.lightblue, `[${Methods.DELETE}]`), colorizeText(Color.yellow, `${url}`));
+
+          users = deleteHandler(url, users);
+
+          code = StatusCode.Successful204;
+
+          break;
+        }
+
+        default:
+          throw new Error(ErrorMsg.NotImplemented);
+      }
+
+      res.writeHead(code);
+    } catch (err) {
+      response = errorHandler(err.message);
+
+      res.writeHead(response.code);
+    } finally {
+      res.end(JSON.stringify(response));
+    }
+  });
 
   server.listen(PORT, () => {
     console.log('Server started on', colorizeText(Color.yellow, `${PORT}`), 'port');
--- src/utils/BodyValidator.ts
+++ src/utils/BodyValidator.ts
@@ -6,8 +6,14 @@ const allFieldsInObject = (body: UserRequest) => UserFields.Username in body
   && UserFields.Age in body
   && UserFields.Hobbies in body;
 
+const isFieldsHaveValidType = ({ username, age, hobbies }: UserRequest) => typeof username === 'string'
+  && typeof age === 'number'
+  && Array.isArray(hobbies);
+
 export const bodyValidator = (body: UserRequest) => {
-  if (!allFieldsInObject(body) || Object.keys(body).length > 3) {
+  if (!allFieldsInObject(body)
+    || Object.keys(body).length > 3
+    || !isFieldsHaveValidType(body)) {
     throw new Error(ErrorMsg.InvalidBody);
   }
 };
